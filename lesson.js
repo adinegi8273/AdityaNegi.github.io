2a2d2e3f8172b63c6a00e2ff7577cc853e15a474
Aditya <adityanegi804@gmail.com>
2025-05-09 19:30:00 +0530

    Add files via upload

---
+++ lesson.js
@@ -0,0 +1,6 @@
+function scrollToTarget1() {
+    document.getElementById('targetID1').scrollIntoView({ behavior: 'smooth' });
+}
+function scrollToTarget2() {
+    document.getElementById('targetID2').scrollIntoView({ behavior: 'smooth' });
+}

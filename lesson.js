03e210510e32faf0c18ecbbceab3f2cf07fdd441
Aditya <adityanegi804@gmail.com>
2026-02-12 23:34:59 +0530

    new portfolio

--- lesson.js
+++
@@ -1,6 +0,0 @@
-function scrollToTarget1() {
-    document.getElementById('targetID1').scrollIntoView({ behavior: 'smooth' });
-}
-function scrollToTarget2() {
-    document.getElementById('targetID2').scrollIntoView({ behavior: 'smooth' });
-}
